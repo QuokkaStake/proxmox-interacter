727bac032350845a8943f8b22f93db8ef776e6d9
Techno Freak <freak12techno@gmail.com>
2023-06-20 22:17:53 +0300

    feat: validate config before startup

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,5 +1,11 @@
 package config
 
+import (
+	"fmt"
+	"main/pkg/types"
+	"net/url"
+)
+
 type Config struct {
 	Log      LogConfig       `toml:"log"`
 	Telegram TelegramConfig  `toml:"telegram"`
@@ -17,12 +23,70 @@ type TelegramConfig struct {
 	Admins []int64 `toml:"admins"`
 }
 
+func (c TelegramConfig) Validate() error {
+	if c.Chat == 0 {
+		return fmt.Errorf("telegram chat not specified")
+	}
+
+	if c.Token == "" {
+		return fmt.Errorf("telegram token not specified")
+	}
+
+	return nil
+}
+
 type ProxmoxConfig struct {
-	URL   string `default:"http://localhost:3000" toml:"url"`
-	User  string `toml:"user"`
-	Token string `toml:"token"`
+	URL         string `default:"http://localhost:8006" toml:"url"`
+	ExternalURL string `toml:"external-url"`
+	User        string `toml:"user"`
+	Token       string `toml:"token"`
+}
+
+func (c ProxmoxConfig) Host() string {
+	if c.ExternalURL != "" {
+		return c.ExternalURL
+	}
+
+	return c.URL
+}
+
+func (c ProxmoxConfig) Validate() error {
+	if c.URL == "" {
+		return fmt.Errorf("proxmox url not specified")
+	}
+
+	if c.User == "" {
+		return fmt.Errorf("proxmox user not specified")
+	}
+
+	if c.Token == "" {
+		return fmt.Errorf("proxmox token not specified")
+	}
+
+	return nil
+}
+
+func (c ProxmoxConfig) GetNodeLink(node types.Node) types.Link {
+	return types.Link{
+		Name: node.Name,
+		Href: fmt.Sprintf(
+			"%s/#v1:0:=%s",
+			c.Host(),
+			url.QueryEscape(node.ID),
+		),
+	}
 }
 
 func (c *Config) Validate() error {
+	if err := c.Telegram.Validate(); err != nil {
+		return fmt.Errorf("error in telegram config: %s", err)
+	}
+
+	for index, proxmoxConfig := range c.Proxmox {
+		if err := proxmoxConfig.Validate(); err != nil {
+			return fmt.Errorf("error in proxmox config #%d: %s", index, err)
+		}
+	}
+
 	return nil
 }
